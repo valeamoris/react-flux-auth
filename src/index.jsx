fe20ec83032c36495ab620f9e109d09fde4b1c7f
valeamoris <243387105@qq.com>
2015-11-26 16:32:32 +0800

    configure the environment

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/11/26.
+ */
+const React = require('react');
+const ReactDom = require('react-dom');
+
+ReactDom.render(
+    <h1>Hello World</h1>,
+    document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2015/11/26.
+ */
+var webpack = require("webpack");
+
+
+module.exports = {
+    entry : "./src/index.jsx",
+    output : {
+        filename: "./js/bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        },{
+            test: /\.jsx$/,
+            loader: 'babel-loader!jsx-loader?harmony'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
